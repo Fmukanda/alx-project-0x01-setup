7c68a80387cbb1dec450b9af3986bf0fda737840
Fernandes Mukanda <fernandezmukanda@gmail.com>
2025-09-22 05:51:00 +0300

    Update index.tsx

---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,12 @@
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+export default Users
